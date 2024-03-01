23035039b6d4dd3bbe6c526abf10a50d1fd9517b
SparrowMike <michal.m_wroblewski@hotmail.com>
2024-03-01 19:09:27 +0800

    fix: 🐛 nvim - lsp related

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,33 +1,40 @@
 return {
-     {
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>gs", ":vert Git<CR>")
+            vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiff)
+        end,
+    },
+    {
         "kdheepak/lazygit.nvim",
         -- optional for floating window border decoration
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
-        config = function ()
-            vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<cr>')
-        end
+        config = function()
+            vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
+        end,
     },
     {
-        'f-person/git-blame.nvim',
+        "f-person/git-blame.nvim",
         config = function()
             vim.g.skip_ts_context_commentstring_module = true
-            vim.keymap.set('n', '<leader>go', '<cmd>GitBlameOpenFileURL<cr>')
-        end
+            vim.keymap.set("n", "<leader>go", "<cmd>GitBlameOpenCommitURL<cr>")
+        end,
     },
     {
-        'lewis6991/gitsigns.nvim',
+        "lewis6991/gitsigns.nvim",
         config = function()
-            require('gitsigns').setup({
+            require("gitsigns").setup({
                 current_line_blame = false,
             })
 
-            vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>')
-            vim.keymap.set('n', '<leader>ga', ':Gitsigns stage_hunk<CR>')
-            vim.keymap.set('n', '<leader>gu', ':Gitsigns undo_stage_hunk<CR>')
-            vim.keymap.set('n', '<leader>gr', ':Gitsigns reset_hunk<CR>')
-            vim.keymap.set('n', '<leader>gb', ':Gitsigns blame_line<CR>')
-        end
-    }
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
+            vim.keymap.set("n", "<leader>ga", ":Gitsigns stage_hunk<CR>")
+            vim.keymap.set("n", "<leader>gu", ":Gitsigns undo_stage_hunk<CR>")
+            vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>")
+            vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>")
+        end,
+    },
 }
